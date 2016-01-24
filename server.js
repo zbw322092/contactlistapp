d10dfe89d9da422785f2bab5343943300079e362
zbw322092 <bz4e14@soton.ac.uk>
2016-01-24 21:19:44 +0800

    Mean Stack Contact List

---
+++ server.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
+var bodyParser = require('body-parser');
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+app.get('/contactlist', function (req, res) {
+	console.log("I receive a GET request");
+
+	db.contactlist.find(function (err, docs) {
+		console.log(docs);
+		res.json(docs);
+	});
+});
+
+app.post('/contactlist', function (req, res) {
+	console.log(req.body);
+	db.contactlist.insert(req.body, function(err, doc) {
+    	res.json(doc);
+  	});
+});
+
+app.delete('/contactlist/:id', function (req, res) {
+	var id = req.params.id;
+	console.log(id);
+	db.contactlist.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+		res.json(doc);
+	})
+});
+
+app.get('/contactlist/:id', function (req, res) {
+	var id = req.params.id;
+	console.log(id);
+	db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+		res.json(doc);
+	});
+});
+
+app.put('/contactlist/:id', function (req, res) {
+	var id = req.params.id;
+	console.log(req.body.name);
+	db.contactlist.findAndModify({
+		query: {_id: mongojs.ObjectId(id)},
+		update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
+		new: true
+	}, function (err, doc) {
+			res.json(doc);
+		});
+});
+
+app.listen(3000, function() {
+	console.log('Server running on port 3000');
+});
